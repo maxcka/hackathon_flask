c74d6f9caa258f8f9d639546e06d5c70f3cc55ba
Max <max@MAX-VOSTRO>
2023-04-15 16:17:43 -0700

    front end stuff

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ def index():
     name = 'Home'
     return render_template('home.html', title=title, name=name)
     
-
-
 @app.route('/events')
 def events():
     title = 'Events'
@@ -22,6 +20,35 @@ def search():
     name = 'Search Results'
     query = request.args.get('query')
     return render_template('search.html', title=title, name=name, query=query)
+
+@app.route('/create')
+def create():
+    title = 'Create Event'
+    name = 'Create Event'
+    return render_template('create.html', title=title, name=name, error=False)
+
+@app.route('/process_event', methods=['POST'])
+def process_event():
+    title = request.form['title']
+    location = request.form['location']
+    date = request.form['date']
+    time = request.form['time']
+    description = request.form['description']
+
+    if not date or not time:
+        error = "Please Fill Out All Fields!"
+        prev_title = "Create Event"
+        prev_name = prev_title
+        return render_template('create.html', title=prev_title, name=name, error=error)
+
+    #new_event = FUNCTION(title=title, location=location,\
+    #                    date=date, time=time, description=description)
+    #db.session.add(new_event)
+    #db.session.commit()
+
+    # redirect after submission
+    return render_template('thanks.html', title=title, location=location, date=date, time=time)
+
 
 if __name__ == '__main__':
     app.run()
