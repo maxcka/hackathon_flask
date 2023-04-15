01c18c702f057f2ce59dd905ded5480d480e28a0
maxcka <76824242+maxcka@users.noreply.github.com>
2023-04-15 14:45:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    title = 'My Flask App'
+    name = 'Home'
+    return render_template('home.html', title=title, name=name)
+    
 
+
+@app.route('/events')
+def events():
+    title = 'Events'
+    name = 'Events'
+    return render_template('events.html', title=title, name=name)
+
+@app.route('/search')
+def search():
+    title = 'Search Results'
+    name = 'Search Results'
+    query = request.args.get('query')
+    return render_template('search.html', title=title, name=name, query=query)
+
+if __name__ == '__main__':
+    app.run()
