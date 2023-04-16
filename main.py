ecf99a7e236ba454cbbee95c71bbf56019880969
max <maxckallas@gmail.com>
2023-04-15 19:53:19 -0700

    finish merge

--- main.py
+++ main.py
@@ -65,13 +65,10 @@ def process_event():
         prev_name = prev_title
         return render_template('create.html', title=prev_title, name=name, error=error)
 
-<<<<<<< HEAD
-=======
     new_event = addEvent(location, description, date, time, title, "Ucla")
     #db.session.add(new_event)
     #db.session.commit()
 
->>>>>>> 9dbfa33d5c15dab2287185b3fb142e9119a85c8c
     # redirect after submission
     return render_template('thanks.html', title=title, location=location, date=date, time=time)
 
@@ -86,52 +83,3 @@ def create_account():
 
 if __name__ == '__main__':
     app.run()
-<<<<<<< HEAD
-=======
-
-
-#from flask import Flask
-#
-#app = Flask(__name__)
-#
-#@app.route("/")
-#def hello_world():
-#    return "<p>Hello, World!</p>"
-#
-#@app.route("/search")
-#def search(request):
-#    locations = request.args.get('location')
-#    if (locations == None):
-#        locations = ["The Hill", "Campus", "Off-Campus", "Bruinwalk"]
-#    types = request.args.get('type')
-#    if (types == None):
-#        types= ["Social Event", "Club Event", "Fundraiser", "Free Food", "Other"]
-#    
-#    from datetime import date, timedelta, time
-#    startDate = request.args.get('startDate')
-#    if(startDate == None):
-#        startDate = date.today()
-#    endDate = request.args.get('endDate')
-#    if(endDate == None):
-#        endDate = startDate + timedelta(days=10)
-#    delta = timedelta(days=1)
-#    dates = []
-#    while startDate <=endDate:
-#        dates.append(startDate)
-#        startDate += delta
-#
-#    #startTime = request.args.get('startTime')
-#    #if(startTime == None):
-#    #    startTime = datetime.time(0,0,0)
-#    # endTime = request.args.get('endTime')
-#    # if(endTime == None):
-#    #     endTime = datetime.time(23,59,59)
-#
-#    events = [] #list of json items - needs to be edited once database has been created
-#    filtered = []
-#    for event in events:
-#        if (event.location in locations and event.type in types and event.date in dates):
-#            filtered.append(event)
-#    return filtered
-
->>>>>>> 9dbfa33d5c15dab2287185b3fb142e9119a85c8c
